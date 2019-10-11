931e76fcafdfc3d0294029b13a757b2314522dce
Brian Schaeflein <jebus27@gmail.com>
2019-10-10 20:26:40 -0500

    retrieve list of all users

--- app.js
+++ app.js
@@ -26,13 +26,12 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/api/users', apiUsersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
-
-app.use('/api/users', apiUsersRouter);
 
 // error handler
 app.use(function(err, req, res, next) {

cd28dfa33a7b09797d9ed6f089766d4e3d6a6177
Brian Schaeflein <jebus27@gmail.com>
2019-10-10 21:02:36 -0500

    configure session and cookie data

--- config.dev.js
+++ config.dev.js
@@ -1,3 +1,8 @@
 var config = {};
 config.mongodb = 'mongodb://localhost/mean-cms';
 module.exports = config;
+
+config.session = {};
+config.cookie = {};
+config.session.secret = '7j&1tH!cr4F*1U';
+config.cookie.domain = 'localhost';
